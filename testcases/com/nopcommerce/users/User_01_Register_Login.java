b10812dbebb62f3c520d36c5960e3c47f0e5ff7d
phuongnta <anhphuong192@gmail.com>
2024-03-02 09:39:29 +0700

    FrameWork Level 1 NopCommerce

--- testcases/com/nopcommerce/users/User_01_Register_Login.java
+++ testcases/com/nopcommerce/users/User_01_Register_Login.java
@@ -10,7 +10,7 @@ import java.time.Duration;
 public class User_01_Register_Login {
     WebDriver driver;
     @BeforeClass
-    public void beforeClass(){
+    public void beforeClass() {
         driver = new FirefoxDriver();
         driver.get("https://demo.nopcommerce.com/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
