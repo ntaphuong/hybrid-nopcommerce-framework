2e7787a08d2b1cc21b0e51168d03c266cd280617
ntaphuong <anhphuong192@gmail.com>
2024-03-13 16:59:46 +0700

    Commit BasePage Selenium Web Browser Function

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -1,4 +1,21 @@
 package commons;
 
+import org.openqa.selenium.WebDriver;
+
 public class BasePage {
+    public void getURL(WebDriver driver, String pageURL){
+        driver.get(pageURL);
+    }
+    public String getPageTitle(WebDriver driver){
+        return  driver.getTitle();
+    }
+    public String getCurrentURL(WebDriver driver){
+        return driver.getCurrentUrl();
+    }
+    public String getPageSource(WebDriver driver){
+        return driver.getPageSource();
+    }
+    public void backToPage(WebDriver driver){
+        driver.navigate().back();
+    }
 }
