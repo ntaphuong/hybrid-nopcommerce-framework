470f0046f7d3a15e9530228570a40f8f9268c63c
ntaphuong <anhphuong192@gmail.com>
2024-03-14 16:33:18 +0700

    Commit BasePage Selenium Web Browser Function

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -1,6 +1,12 @@
 package commons;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
     public void getURL(WebDriver driver, String pageURL){
@@ -18,4 +24,23 @@ public class BasePage {
     public void backToPage(WebDriver driver){
         driver.navigate().back();
     }
+    public void forwardToPage(WebDriver driver) {driver.navigate().forward();}
+    public void refreshCurrentPage(WebDriver driver){driver.navigate().refresh();}
+    public Alert waitForAlertPresence(WebDriver driver){
+        return new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.alertIsPresent());
+    }
+    public void exceptToAlert(WebDriver driver){
+        waitForAlertPresence(driver).accept();
+    }
+    public void cancelToAlert(WebDriver driver){
+        waitForAlertPresence(driver).dismiss();
+    }
+    public String getTextInAlert (WebDriver driver){
+        return waitForAlertPresence(driver).getText();
+    }
+    public void sendkeyToAlert (WebDriver driver, String KeysToSend){
+        waitForAlertPresence(driver).sendKeys(KeysToSend);
+    }
+    // Windows
+
 }
