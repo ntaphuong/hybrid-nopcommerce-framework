9c56f1a603677fa531783b270be32dfa5afc74e0
phuongnta <anhphuong192@gmail.com>
2024-03-19 21:38:50 +0700

    FrameWork Level 1 NopCommerce, continue BasePage

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -1,9 +1,11 @@
 package commons;
 
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -26,21 +28,35 @@ public class BasePage {
     }
     public void forwardToPage(WebDriver driver) {driver.navigate().forward();}
     public void refreshCurrentPage(WebDriver driver){driver.navigate().refresh();}
-    public Alert waitForAlertPresence(WebDriver driver){
+    public Alert waitAlertPresence(WebDriver driver){
         return new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.alertIsPresent());
     }
     public void exceptToAlert(WebDriver driver){
-        waitForAlertPresence(driver).accept();
+        waitAlertPresence(driver).accept();
     }
     public void cancelToAlert(WebDriver driver){
-        waitForAlertPresence(driver).dismiss();
+        waitAlertPresence(driver).dismiss();
     }
     public String getTextInAlert (WebDriver driver){
-        return waitForAlertPresence(driver).getText();
+        return waitAlertPresence(driver).getText();
     }
-    public void sendkeyToAlert (WebDriver driver, String KeysToSend){
-        waitForAlertPresence(driver).sendKeys(KeysToSend);
+    public void sendKeyToAlert (WebDriver driver, String KeysToSend){
+        waitAlertPresence(driver).sendKeys(KeysToSend);
     }
     // Windows
-
+    public void clickToElement(WebDriver driver, String locator){
+        driver.findElement(By.xpath(locator)).click();
+    }
+    public void sendKeyToElement(WebDriver driver, String locator, String keysToSend){
+        driver.findElement(By.xpath(locator)).sendKeys(keysToSend);
+    }
+    public void selectItemInDropdown(WebDriver driver, String locator, String textItem){
+        new Select(driver.findElement(By.xpath(locator))).selectByVisibleText(textItem);
+    }
+    public String getSelectedItemInDropdown(WebDriver driver, String locator){
+        return new Select(driver.findElement(By.xpath(locator))).getFirstSelectedOption().getText();
+    }
+    public boolean isDropdownMultiple(WebDriver driver, String locator){
+        return new Select(driver.findElement(By.xpath(locator))).isMultiple();
+    }
 }
