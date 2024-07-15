9e2ecadc9537d5cf198dcaecabb23638fefe3c69
ntaphuong <anhphuong192@gmail.com>
2024-07-15 15:03:35 +0700

    Level_13_Assert_Verify

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.Reporter;
 
 import java.time.Duration;
 import java.util.Random;
@@ -52,19 +53,41 @@ public class BaseTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         return driver;
     }
-    protected void verifyTrue(boolean condition){
-        try{
+    protected void assertTrue(boolean condition){
+
+    }
+    private boolean verifyTrue(boolean condition){
+        boolean status = true;
+        try {
             Assert.assertTrue(condition);
-        }catch (Exception e){
+        } catch (Throwable e) {
+            status = false;
 
+           // VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
         }
-
-
+        return status;
     }
-    protected void verifyFalse(boolean condition){
-
+    protected boolean verifyFalse(boolean condition){
+        boolean status = true;
+        try {
+            Assert.assertFalse(condition);
+        } catch (Throwable e) {
+            status = false;
+          // VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+        }
+        return status;
     }
-    protected void verifyEquals(Object actual, Object expected){
-
+    protected boolean verifyEquals(Object actual, Object expected){
+        boolean status = true;
+        try {
+            Assert.assertEquals(actual, expected);
+        } catch (Throwable e) {
+            status = false;
+            //VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+        }
+        return status;
     }
 }
