95a8790766f6498f9bc32366c8f6332be98fa326
phuongnta <anhphuong192@gmail.com>
2024-08-13 22:37:51 +0700

    Level_16_AllureReport

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -3,15 +3,18 @@ package commons;
 //import org.apache.commons.logging.Log;
 //import org.apache.commons.logging.LogFactory;
 //Loj4j2.3.1
+
 import org.apache.logging.log4j.LogManager;
-import  org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.Reporter;
+import org.testng.annotations.BeforeSuite;
 
+import java.io.File;
 import java.time.Duration;
 import java.util.Random;
 
@@ -108,5 +111,26 @@ public class BaseTest {
 
     public WebDriver getDriver() {
         return  driver;
+    }
+    @BeforeSuite
+    public void deleteReportFolder(){
+        deleteAllFileInFolder("htmlAllure");
+    }
+
+    public void deleteAllFileInFolder(String folderName) {
+        try {
+            String pathFolderDownload = GlobalConstants.PROJECT_PATH + File.separator + folderName;
+            File file = new File(pathFolderDownload);
+            File[] listOfFiles = file.listFiles();
+            if (listOfFiles.length != 0) {
+                for (int i = 0; i < listOfFiles.length; i++) {
+                    if (listOfFiles[i].isFile() && !listOfFiles[i].getName().equals("environment.properties")) {
+                        new File(listOfFiles[i].toString()).delete();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
     }
 }
